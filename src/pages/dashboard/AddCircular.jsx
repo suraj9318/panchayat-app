8564ae4fddd93355a9ae0b9d9de987c28e2a7543
suraj <surajmishra9318@gmail.com>
2023-04-01 23:07:26 +0530

    add circular done

--- src/pages/dashboard/AddCircular.jsx
+++ src/pages/dashboard/AddCircular.jsx
@@ -6,38 +6,64 @@ import SmallSidebar from '../../components/SmallSidebar'
 
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { toast } from 'react-toastify'
 
 const AddCircular = () => {
     const formik = useFormik({
-        initialValues:{
-            circularNumber : '', 
-            subject : '', 
-            category : '', 
-            language : '', 
-            description : '', 
-            issuedOn : '', 
-            issuedBy : '', 
-            file : '', 
-            
+        initialValues: {
+            circularNumber: '',
+            subject: '',
+            category: '',
+            language: '',
+            description: '',
+            issuedOn: '',
+            issuedBy: '',
+            file: '',
         },
         validationSchema: Yup.object({
-            circularNumber:   Yup.string()
-            .required("Please enter circular number")
-            .matches(
-                /^.*(?=.{3,}).*$/,
-                "Please enter at least 3 digit"
-            ),
-            subject:   Yup.string()
-            .required("Please enter the subject title"),
+            circularNumber: Yup.string()
+                .required("Please enter circular number")
+                .matches(
+                    /^.*(?=.{3,}).*$/,
+                    "Please enter at least 3 digit"
+                ),
+            subject: Yup.string()
+                .required("Please enter the subject title"),
             category: Yup.string().required("Please select category type"),
             language: Yup.string().required("Please select the language"),
             description: Yup.string().required("Please enter file description"),
             issuedOn: Yup.date().required("Please enter Issued On"),
             issuedBy: Yup.string().required("Please enter Issued By"),
+            file: Yup.string().required("Please select your File"),
         }),
 
-        onSubmit: values => {
-            console.log(values)
+        onSubmit: (values,{ resetForm }) => {
+            let formdata = new FormData()
+            formdata.append('file',values.file)
+            formdata.append('circularNumber',values.circularNumber)
+            formdata.append('subject',values.subject)
+            formdata.append('category',values.category)
+            formdata.append('language',values.language)
+            formdata.append('description',values.description)
+            formdata.append('issuedOn',values.issuedOn)
+            formdata.append('issuedBy',values.issuedBy)
+
+            const requestOptions = {
+                method: 'POST',
+                body: formdata
+            };
+            fetch('http://localhost:5000/api/v1/addCircular', requestOptions)
+            .then(response =>{
+                if(response.status === 201){
+                    toast.success("Record Added Successfully")
+                    resetForm();
+                }
+                else{
+                    toast.error("Something went wrong !!")
+                }
+            })
+           
+
         }
 
     })
@@ -55,29 +81,29 @@ const AddCircular = () => {
                                 <div className='form-center'>
                                     <label htmlFor="circularNumber" className='form-label' >Circular Number</label>
                                     <input type="number"
-                                    placeholder='Enter Circular Number'
-                                    value={formik.values.circularNumber}
-                                    onChange={formik.handleChange} 
-                                    id='circularNumber' 
-                                    name='circularNumber'
-                                    className='form-input' />
+                                        placeholder='Enter Circular Number'
+                                        value={formik.values.circularNumber}
+                                        onChange={formik.handleChange}
+                                        id='circularNumber'
+                                        name='circularNumber'
+                                        className='form-input' />
                                 </div>
                                 {formik.errors.circularNumber && formik.touched.circularNumber && (
                                     <p className='error'>{formik.errors.circularNumber}</p>
                                 )}
 
 
-                              
+
 
                                 <div className='form-center'>
                                     <label htmlFor="" className='form-label' >Subject</label>
                                     <input type="text"
-                                     value={formik.values.subject}
-                                     onChange={formik.handleChange} 
-                                     id='subject' 
-                                     name='subject'
+                                        value={formik.values.subject}
+                                        onChange={formik.handleChange}
+                                        id='subject'
+                                        name='subject'
 
-                                    placeholder='Enter Subject' className='form-input' />
+                                        placeholder='Enter Subject' className='form-input' />
                                 </div>
                                 {formik.errors.subject && formik.touched.subject && (
                                     <p className='error'>{formik.errors.subject}</p>
@@ -86,10 +112,10 @@ const AddCircular = () => {
                                 <div className='form-center'>
                                     <label htmlFor="category" className='form-label' >Category</label>
                                     <select
-                                    value={formik.values.category}
-                                    onChange={formik.handleChange} 
-                                    id='category' 
-                                    name='category'
+                                        value={formik.values.category}
+                                        onChange={formik.handleChange}
+                                        id='category'
+                                        name='category'
                                         className='form-select'
                                     >
                                         <option value="">Select</option>
@@ -105,12 +131,12 @@ const AddCircular = () => {
 
                                 <div className='form-center'>
                                     <label htmlFor="" className='form-label' >File Language</label>
-                                    <select 
-                                    value={formik.values.language}
-                                    onChange={formik.handleChange} 
-                                    id='language' 
-                                    name='language'
-                                    className='form-select'>
+                                    <select
+                                        value={formik.values.language}
+                                        onChange={formik.handleChange}
+                                        id='language'
+                                        name='language'
+                                        className='form-select'>
                                         <option value="">Select</option>
                                         <option value="Hindi">Hindi</option>
                                         <option value="English">English</option>
@@ -123,12 +149,12 @@ const AddCircular = () => {
 
                                 <div className='form-center'>
                                     <label htmlFor="" className='form-label' >File Description</label>
-                                    <textarea rows={5} className='form-input' 
-                                    value={formik.values.description}
-                                    onChange={formik.handleChange} 
-                                    id='description' 
-                                    name='description'
-                                    
+                                    <textarea rows={5} className='form-input'
+                                        value={formik.values.description}
+                                        onChange={formik.handleChange}
+                                        id='description'
+                                        name='description'
+
                                     />
                                 </div>
                                 {formik.errors.description && formik.touched.description && (
@@ -138,11 +164,11 @@ const AddCircular = () => {
 
                                 <div className='form-center'>
                                     <label htmlFor="" className='form-label' >Issued On</label>
-                                    <input type="date"  className='form-input'
-                                    value={formik.values.issuedOn}
-                                    onChange={formik.handleChange} 
-                                    id='issuedOn' 
-                                    name='issuedOn'
+                                    <input type="date" className='form-input'
+                                        value={formik.values.issuedOn}
+                                        onChange={formik.handleChange}
+                                        id='issuedOn'
+                                        name='issuedOn'
 
                                     />
                                 </div>
@@ -153,11 +179,11 @@ const AddCircular = () => {
 
                                 <div className='form-center'>
                                     <label htmlFor="" className='form-label' >Issued By</label>
-                                    <textarea rows="5" className='form-input' 
-                                    value={formik.values.issuedBy}
-                                    onChange={formik.handleChange} 
-                                    id='issuedBy' 
-                                    name='issuedBy'
+                                    <textarea rows="5" className='form-input'
+                                        value={formik.values.issuedBy}
+                                        onChange={formik.handleChange}
+                                        id='issuedBy'
+                                        name='issuedBy'
 
                                     />
                                 </div>
@@ -167,19 +193,25 @@ const AddCircular = () => {
 
 
                                 <div className='form-center'>
-                                    <label htmlFor="" className='form-label' 
-                                    
-                                    
+                                    <label htmlFor="" className='form-label'
                                     >Attach File</label>
-                                    <input type="file"  className='form-input' />
+                                    <input type="file" className='form-input'
+                                        name='file' id='file'
+                                        onChange={(event) => {
+                                            formik.setFieldValue("file", event.currentTarget.files[0]);
+                                        }}
+                                    />
                                 </div>
-                                
+                                {formik.errors.file && formik.touched.file && (
+                                    <p className='error'>{formik.errors.file}</p>
+                                )}
+
                                 <div className='form-row'>
                                     <button type="submit" className="btn signup-btn">Submit</button>
                                 </div>
 
                             </form>
-                                
+
                         </div>
                     </div>
                 </div>
